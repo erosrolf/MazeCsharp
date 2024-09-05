73cab72c1d1c2a4887745cf6166dcfc10ffc3847
Erosrolf <Erosrolf@gmail.com>
2024-09-05 12:13:23 +0300

    Init Avalonia project

---
+++ Maze.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Maze.App.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Maze.Core/Models/Maze.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace Maze.Core.Models;
+
+public struct Maze
+{
+    public Vector2 Size;
+    public bool[,] RightWalls;
+    public bool[,] BottomWalls;
+}
